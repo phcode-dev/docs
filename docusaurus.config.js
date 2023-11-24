5d911c11780fdefffc1435dcde10684375ed08fa
Charly Abraham <charly@core.ai>
2023-11-24 17:03:49 +0530

    chore: update org and project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,8 +21,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'phcode.dev', // Usually your GitHub org/user name.
-  projectName: 'phcode', // Usually your repo name.
+  organizationName: 'phcode-dev', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
   deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
