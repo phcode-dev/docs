91c75ff66e89da6344a2e84914bfabc1968e4eb7
Charly Abraham <charly@core.ai>
2023-11-24 16:54:21 +0530

    chore: change base url to  / for testing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://docs.phcode.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
   trailingSlash :false,
 
   // GitHub pages deployment config.
