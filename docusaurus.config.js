a0adb605fbfcddd60037e9b78d2b82371f28f58d
Charly Abraham <charly@core.ai>
2023-11-24 19:21:59 +0530

    feat: updated colours and urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,14 +46,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/phcode-dev/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/phcode-dev/docs',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -68,9 +68,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Phoenix Code',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Phoenix Code',
           src: 'img/logo.svg',
         },
         items: [
@@ -104,17 +104,21 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'Discord',
+                href: 'https://discord.gg/rBpTBPttca',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: 'reddit',
+                href: 'https://www.reddit.com/r/brackets/',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/phcodedev/',
               },
+              {
+                label: 'YouTube',
+                href: 'https://www.youtube.com/channel/UCNK2a8DKqPQQe3GlfTk-RHg',
+              }
             ],
           },
           {
@@ -126,12 +130,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/phcode-dev/',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} core.ai scientific technologies private limited"`,
       },
       prism: {
         theme: prismThemes.github,
