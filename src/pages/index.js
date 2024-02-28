4bb0633878671ffb7b09b3841130df5cf3f9d89e
Charly Abraham <charly@core.ai>
2024-02-28 18:12:04 +0530

    chore: update meta info

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Documentation for Phoenix Code editor <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
